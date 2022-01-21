55c4c614973db7bc2e4e029e03b80aedd47bf0d2
Ayoub <ubmagh@gmail.com>
2022-01-21 11:28:53 +0100

    7) /movies returning all file date

--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    const fs = require('fs')
+    res.json( JSON.parse( fs.readFileSync('./var/movies_reduit.json', {encoding:'utf8', flag:'r'}) ) );
+
 });
 
 module.exports = router;
